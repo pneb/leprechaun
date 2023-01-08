ccfaa59f97e79dc72e581aa31d7f57726c623449
Bernward Sanchez <sanzbernward@gmail.com>
2023-01-08 21:44:36 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='leprechaun',
+    version='0.1.0',
+    packages=['leprechaun'],
+    description='A lightweight and efficient asyncio-like library for Python 3',
+    author='Bernward Sanchez',
+    author_email='contact@bern.codes',
+    license='MIT',
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+    ],
+)
+
